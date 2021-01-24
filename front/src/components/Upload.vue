2d2aa05ba9b0e271cf3e4ed4614d3371bf3dff9c
SomaSekimoto <0421.soma@gmail.com>
2021-01-24 23:09:47 +0900

    fix: upload.vue

---
+++ front/src/components/Upload.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <p>拡張子.xlsxのファイル</p>
+    <input type="file" @change="uploadFile" />
+  </div>
+  <!-- <button style="margin: 3em;" @click="uploadFile">アップロード</button> -->
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Upload",
+  props: {
+    msg: String
+  },
+  data: () => ({}),
+  methods: {
+    uploadFile(e) {
+      console.log("aaa");
+      console.log(e.target.files[0]);
+      axios.get("/fetch", { params: e.target.files[0] });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Upload msg="ファイルをアップロードしてください。" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Upload from "@/components/Upload.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    Upload
   }
-}
+};
 </script>
